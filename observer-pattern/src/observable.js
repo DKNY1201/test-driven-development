473041aceddc208e5a6ae776f40f0e70491efc07
Quy Tran <quytran288@gmail.com>
2018-02-09 20:05:20 +0700

    Apply some testing

---
+++ observer-pattern/src/observable.js
@@ -0,0 +1,16 @@
+console.log(123);
+tddjs.namespace("util");
+
+(function () {
+    function Observable() {
+        this.observers = []
+    }
+
+    tddjs.util.Observable = Observable;
+
+    function addObserver(observer) {
+        this.observers.push(observer);
+    }
+    Observable.prototype.addObserver = addObserver;
+}())
+
