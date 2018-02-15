4728270ab9583481dcc97736a2a3a076eb2e456a
Quy Tran <quytran288@gmail.com>
2018-02-15 16:50:04 +0700

    - Implement tdd.js with full features - Implement ajax's create function

--- ajax/src/ajax.js
+++ ajax/src/ajax.js
@@ -1,3 +1,37 @@
-/**
- * Created by Bi on 2/15/18.
- */
+// tddjs.namespace("ajax");
+
+(function () {
+    var xhr;
+    var ajax = tddjs.namespace("ajax");
+    var options = [
+        function () {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        },
+        function () {
+            return new XMLHttpRequest();
+        }
+    ];
+
+    for (var i = 0, l = options.length; i < l; i++) {
+        try {
+            xhr = options[i]();
+
+            if (typeof xhr.readyState == "number" &&
+                tddjs.isHostMethod(xhr, "open") &&
+                tddjs.isHostMethod(xhr, "send") &&
+                tddjs.isHostMethod(xhr, "setRequestHeader")) {
+                ajax.create = options[i];
+                break;
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    return null;
+})();
+
+
+tddjs.namespace("ajax").isHostMethod = function () {
+
+}
